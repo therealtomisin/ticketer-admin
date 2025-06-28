a76ddab1ad48336fd64814a55779f0b88d3d4fbd
therealtomisin <timilehineribake@yahoo.com>
2025-06-28 21:02:10 +0100

    chore: update env

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -2,9 +2,7 @@ import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client/core
 import { setContext } from '@apollo/client/link/context'
 
 const httpLink = createHttpLink({
-  // uri: import.meta.env.BASE_URL, // adjust as needed
-  uri: 'http://localhost:4003/graphql', // adjust as needed
-  // uri: 'https://mysite-856n.onrender.com/graphql', // adjust as needed
+  uri: import.meta.env.VITE_BASE_URL,
 })
 
 const authLink = setContext((_, { headers }) => {
